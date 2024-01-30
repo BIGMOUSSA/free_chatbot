78d75b9eaba115a5ed63d5d45c8f58bee045c179
Moussa DIALLO <diallomous@gmail.com>
2024-01-30 20:14:54 +0000

    adding webchat

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,16 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+# actions.py
 
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from my_request import send_request_and_store_result
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionGreet(Action):
+    def name(self) -> Text:
+        return "action_nlg"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        latest_message = tracker.latest_message.get('text', '')
+        answer = send_request_and_store_result(query = latest_message)
+        dispatcher.utter_message(f"{answer}")
+        return []
---
+++ my_request.py
@@ -0,0 +1,37 @@
+import requests
+import json
+def send_request_and_store_result(query):
+    # URL of the FASPI API endpoint you want to query
+    faspi_url = "http://localhost:8000/predict"
+
+    # Parameters to include in the request, if any
+    data = {
+        "text" : query
+        # Add more parameters as needed
+    }
+
+    try:
+        # Sending GET request to the FASPI API
+        response = requests.post(faspi_url, json=data)
+
+        # Checking if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Assuming the response is JSON, you can parse it and store it
+            result = response.json()
+
+            # Store the result in a file, database, or any other desired storage
+            print("Request successful. Result stored.")
+            return result["answer"]
+            #with open('faspi_result.json', 'w') as f:
+            #    json.dump(result, f)
+
+            
+        else:
+            print(f"Error: Failed to retrieve data. Status code: {response.status_code}")
+
+    except requests.RequestException as e:
+        print(f"Error: Failed to make request - {e}")
+
+if __name__ == "__main__":
+    query = "how was industy activity in 2022"
+    send_request_and_store_result(query=query)
