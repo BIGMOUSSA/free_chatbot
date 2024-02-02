1e0ce0f66bf34f1c81c39c0983ea1388478dc6a4
Moussa DIALLO <diallomous@gmail.com>
2024-02-02 12:07:54 +0000

    adding fallback

--- actions/actions.py
+++ actions/actions.py
@@ -14,3 +14,13 @@ class ActionGreet(Action):
         answer = send_request_and_store_result(query = latest_message)
         dispatcher.utter_message(f"{answer}")
         return []
+
+class ActionDefaultFallback(Action):
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_default")
+        return []
