974fcbe2669f9fd91b9db4414fe89a5ff10b9476
Moussa DIALLO <diallomous@gmail.com>
2024-02-03 18:15:51 +0000

    send bert version

--- actions/actions.py
+++ actions/actions.py
@@ -1,39 +1,27 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+# actions.py
 
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from my_request import send_request_and_store_result
 
-class ActionHandleLowConfidence(Action):
+class ActionGreet(Action):
     def name(self) -> Text:
-        return "action_handle_low_confidence"
+        return "action_nlg"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        # Custom logic for handling low confidence
-        dispatcher.utter_message("I'm sorry, but I'm not sure about that. Can you please rephrase?")
+        latest_message = tracker.latest_message.get('text', '')
+        #answer = send_request_and_store_result(query = latest_message)
+        answer =f"{latest_message} api bientot disponible!"
+        dispatcher.utter_message(f"{answer}")
+        return []
+
+class ActionDefaultFallback(Action):
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(template="utter_default")
         return []
