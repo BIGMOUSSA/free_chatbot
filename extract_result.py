2a3c5282084275f4390f7f2c845a494ad3f389dc
Moussa DIALLO <diallomous@gmail.com>
2024-02-04 21:44:26 +0000

    ep200_lr=0.01_ena_fa_pred=false_ngram_1_4_max_his_5

---
+++ extract_result.py
@@ -0,0 +1,16 @@
+import json
+import pandas as pd
+
+def reporting(folder, rubrique):
+    with open(f"gridresults/{folder}/{rubrique}_report.json", "r") as file :
+        report = json.load(file)
+    pd.DataFrame(report).to_excel(f"gridresults/{folder}_{rubrique}.xlsx")
+
+
+if __name__ == "__main__":
+    reporting("config-labse", "intent")
+    reporting("config-labse", "response_selection")
+    reporting("config-regex", "intent")
+    reporting("config-regex", "response_selection")
+    reporting("config-spacy", "intent")
+    reporting("config-spacy", "response_selection")
